2cb70653bd3ee4c63a7f1f5e9fbf4ee22168b597
MatteoCantafio25 <mattyteo004@gmail.com>
2024-02-01 19:57:20 +0100

    added footer top

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script>
 import AppHeader from "./components/AppHeader.vue"
 import AppMainContent from "./components/AppMainContent.vue"
+import AppFooterTop from "./components/Footer/AppFooterTop.vue"
+
 export default {
 
   name: "Vite-Comics",
   components: {
     AppHeader,
     AppMainContent,
+    AppFooterTop,
   },
 };
 </script>
@@ -14,9 +17,16 @@ export default {
 <template>
   <AppHeader />
   <AppMainContent />
+  <AppFooterTop />
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 body {
   font-family: 'Open Sans', Open Sans Condensed;
 }
