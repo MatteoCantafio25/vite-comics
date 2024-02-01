0c79b1ca0810e06768e7cdf48bfbb3d479c18dfd
MatteoCantafio25 <mattyteo004@gmail.com>
2024-02-01 20:05:02 +0100

    added spam section

---
+++ src/components/AppSpam.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "AppSpam",
+};
+</script>
+
+<template>
+    <div class="spam"></div>
+</template>
+
+<style scoped>
+.spam {
+    height: 120px;
+    background-color: #0282F9;
+}
+</style>
